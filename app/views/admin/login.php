a99cd6e30b01d0fc18c6852303fce3c3fb5df8a0
npapratovic <nikola.papratovic@gmail.com>
2014-12-24 20:25:21 +0100

    login screen

--- app/views/admin/login.php
+++ app/views/admin/login.php
@@ -1,9 +1,38 @@
-<h1>Login</h1>
+ 
 
-<form action="" method="post">
 
-<p>Username<br /><input type='text' name='username'></p>
-<p>Password<br /><input type='password' name='password'></p>
-<p><input type='submit' name='submit' value='Login'></p>
+<section id="login-container animated fadeIn">
 
-</form>
+        <div class="row">
+            <div class="col-md-3" id="login-wrapper">
+                <div class="panel panel-primary animated flipInY">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">     
+                           Prijava u sustav
+                        </h3>      
+                    </div>
+                    <div class="panel-body">
+                         <form class="form-horizontal" role="form" action="" method="post">
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" id="username" name="username" placeholder="Korisničko ime">
+                                 </div>
+                            </div>
+                            <div class="form-group">
+                               <div class="col-md-12">
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                                 </div>
+                            </div>
+                            <div class="form-group">
+                               <div class="col-md-12">
+                                    <input class="btn btn-primary btn-block" type='submit' name='submit' value='Prijava'> 
+                                    <hr>
+                                 </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
